ebfc24605decc22d16da8d48711601cd9f3eb3d0
PhucHung <nph04022004@gmail.com>
2025-12-25 13:52:50 +0700

    Fix: Add Highest Precedence CORS Filter and fix Server Start

--- Backend/src/main/java/com/devansh/config/SecurityConfig.java
+++ Backend/src/main/java/com/devansh/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.devansh.config;
 
 import com.devansh.model.enums.Role;
-import com.devansh.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
